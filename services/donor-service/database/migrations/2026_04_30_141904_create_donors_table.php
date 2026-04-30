3caeac74a815e747e00e565eb91e5f3600052196
albimulfi <albi.mulfi@gmail.com>
2026-04-30 21:46:26 +0700

    refactor: remove duplicate donor migration file

--- services/donor-service/database/migrations/2026_04_30_141904_create_donors_table.php
+++ services/donor-service/database/migrations/2026_04_30_141904_create_donors_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('donors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('blood_type');
+            $table->string('phone');
             $table->timestamps();
         });
     }
