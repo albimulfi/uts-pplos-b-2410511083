63075f15632f780b3ffa0234d6cfdc290bf486d7
albimulfi <albi.mulfi@gmail.com>
2026-05-01 00:57:48 +0700

    feat: implement API gateway routing to donor service

--- services/api-gateway/routes/gatewayRoutes.js
+++ services/api-gateway/routes/gatewayRoutes.js
@@ -26,3 +26,27 @@ router.post("/auth/login", async (req, res) => {
         res.status(500).json({ message: "Auth service error" });
     }
 });
+
+// donor
+router.get("/donors", async (req, res) => {
+    try {
+        const response = await axios.get(
+            `${process.env.DONOR_SERVICE}/api/donors`
+        );
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).json({ message: "Donor service error" });
+    }
+});
+
+router.post("/donors", async (req, res) => {
+    try {
+        const response = await axios.post(
+            `${process.env.DONOR_SERVICE}/api/donors`, 
+            req.body
+        );
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).json({ message: "Donor service error" });
+    }
+});
