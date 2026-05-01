655dfa5ed965da9a1c022b61ac2f7f004d9ebff0
albimulfi <albi.mulfi@gmail.com>
2026-05-01 17:00:41 +0700

    feat: implement rate limiting in api gateway

--- services/api-gateway/index.js
+++ services/api-gateway/index.js
@@ -3,12 +3,21 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const rateLimit = require("express-rate-limit");
 
 app.use(cors());
 app.use(express.json());
 
 const gatewayRoutes = require("./routes/gatewayRoutes");
 app.use("/", gatewayRoutes);
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 mnt
+    max: 10, // max 10 request
+    message: { message: "Terlalu banyak request, coba lagi nanti" }
+});
+
+app.use(limiter);
 
 app.listen(process.env.PORT, () => {
     console.log("API Gateway jalan di port " + process.env.PORT);
