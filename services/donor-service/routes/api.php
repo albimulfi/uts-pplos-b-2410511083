5228932cbdd10d795df0c5e6d186f66a46f50aa3
albimulfi <albi.mulfi@gmail.com>
2026-05-01 13:57:10 +0700

    feat: add API routes for hospital, request, donation

--- services/donor-service/routes/api.php
+++ services/donor-service/routes/api.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DonorController;
+use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\RequestBloodController;
+use App\Http\Controllers\DonationController;
 
 Route::get('/donors', [DonorController::class, 'index']);
 Route::post('/donors', [DonorController::class, 'store']);
+
+Route::get('/hospitals', [HospitalController::class, 'index']);
+Route::post('/hospitals', [HospitalController::class, 'store']);
+
+Route::get('/requests', [RequestBloodController::class, 'index']);
+Route::post('/requests', [RequestBloodController::class, 'store']);
+
+Route::get('/donations', [DonationController::class, 'index']);
+Route::post('/donations', [DonationController::class, 'store']);
